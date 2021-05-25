31f9f65bbefd4c7d06afddf579b2482b0dbf7847
GohZHong <gohzhong@gmail.com>
2021-05-25 18:51:32 +0800

    Edit

--- controllers/register.js
+++ controllers/register.js
@@ -1,15 +1,16 @@
 const handleRegister = (req, res, db, bcrypt)=>{
   const { email, name, password, colour} = req.body;
   const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-    .into('login')
-    .returning('email')
-    .then(loginEmail => {
-      return trx('users')
+  if (email && password) {
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
         .returning('*')
         .insert({
           email: loginEmail[0],
@@ -20,12 +21,16 @@ const handleRegister = (req, res, db, bcrypt)=>{
         .then(user => {
           res.json(user[0]);
         })
-    })
-    .then(trx.commit)
-    .catch(trx.rollback)
-  })
-  .catch(err => res.status(400).json('unable to register'))
-  
+        .catch((err)=>res.status(400).json('Error appending database', err))
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+      })
+      .catch(()=>res.status(400).json(false))
+    .catch(()=>res.status(400).json(false))
+  } else {
+    return res.json(false)
+  }
   // const { name, email, password, colour } = req.body;
   // if(email && password) {
   //   bcrypt.hash(password, null, null, function(err, hash) {
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,37 +1,37 @@
 const handleSignIn = (req,res, db, bcrypt) => {
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', req.body.email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
-      } else {
-        res.status(400).json('wrong credentials')
-      }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))  
-  // const { email, password } = req.body;
-    // db.select('email','hash').from('login')
-    // .where({'email': email})
-    // .then(data => { 
-    //   bcrypt.compare(password, data[0].hash, function(err, resp) {
-    //     if(resp) {
-    //       return db('users').select('*')
-    //       .where({'email': email})
-    //       .then(user => res.json(user[0]))
-    //       .catch(err => res.status(400).json('Error retrieving user data ', err))
-    //     } else {
-    //       res.json(false);
-    //     }
-    //   })
-    //   .catch(()=>res.json("Error comparing passwords"))
-    // })
-    // .catch(() =>res.json(false))
+  .where('email', '=', req.body.email)
+  .then(data => {
+    const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+    if (isValid) {
+      return db.select('*').from('users')
+        .where('email', '=', req.body.email)
+        .then(user => {
+          res.json(user[0])
+        })
+        .catch(err => res.status(400).json('Error retrieving user data ', err))
+    } else {
+      res.json(false);
+    }
+  })
+  .catch(() =>res.json(false))
+// const { email, password } = req.body;
+// db.select('email','hash').from('login')
+// .where({'email': email})
+// .then(data => { 
+//   bcrypt.compare(password, data[0].hash, function(err, resp) {
+//     if(resp) {
+//       return db('users').select('*')
+//       .where({'email': email})
+//       .then(user => res.json(user[0]))
+//       .catch(err => res.status(400).json('Error retrieving user data ', err))
+//     } else {
+//       res.json(false);
+//     }
+//   })
+//   .catch(()=>res.json("Error comparing passwords"))
+// })
+// .catch(() =>res.json(false))
 }
 
 module.exports = {
